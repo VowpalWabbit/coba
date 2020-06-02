4b9961e0d76dfd782ffef6eaa96cfa48efa6fb59
Mark Rucker <rucker.mark@gmail.com>
2020-06-02 13:32:34 -0400

    better symmetry in names and classes

--- bbench/games.py
+++ bbench/games.py
@@ -10,15 +10,14 @@ from typing import Iterable, Sequence, List, Generic, TypeVar, Any, Union, cast
 
 #reward type
 R  = float
-
 #feature type (in theory we chould probably be more specific than Any but using unions with base types is a huge headache)
 #scratch what I say above, implicit type conversion works with union when F is used inside of a "Sequence" rather than a "List"
 F = Union[str,float,Sequence[Union[str,float]]]
 
 #game round
-T = TypeVar('T', bound=BanditRound) # pylint: disable=used-before-assignment
+T = TypeVar('T', bound=Round) # pylint: disable=used-before-assignment
 
-class BanditRound:
+class Round:
     def __init__(self, action_features: Sequence[F], action_rewards: Sequence[R]) -> None:
         self._action_features = action_features
         self._action_rewards = action_rewards
@@ -31,7 +30,7 @@ class BanditRound:
     def action_rewards(self) -> Sequence[R]:
         return self._action_rewards
 
-class ContextualBanditRound(BanditRound):
+class ContextRound(Round):
     def __init__(self, context_features: F, action_features: Sequence[F], action_rewards: Sequence[R]):
         super().__init__(action_features, action_rewards)
         self._context_features = context_features
@@ -40,20 +39,28 @@ class ContextualBanditRound(BanditRound):
     def context_features(self) -> F:
         return self._context_features
 
-class BanditGame(Generic[T]):
+class Game:
 
+    def __init__(self, rounds: Iterable[Round]) -> None:
+        self._rounds = rounds
+
+    @property
+    def rounds(self) -> Iterable[Round]:
+        return self._rounds
+
+class ContextGame(Game):
     @staticmethod
-    def from_classifier_data(features: Sequence[F], labels: Sequence[Union[str,float]]) -> BanditGame: # pylint: disable=undefined-variable
+    def from_classifier_data(features: Sequence[F], labels: Sequence[Union[str,float]]) -> ContextGame: # pylint: disable=undefined-variable
         rounds  = []
         actions = list(set(labels))
 
         for context_features, rewarded_action in zip(features, labels):
-            rounds.append(ContextualBanditRound(context_features, actions, [int(rewarded_action==a) for a in actions] ))
+            rounds.append(ContextRound(context_features, actions, [int(rewarded_action==a) for a in actions] ))
 
-        return BanditGame(rounds)
+        return ContextGame(rounds)
 
     @staticmethod
-    def from_csv_reader(csv_reader: Iterable[List[str]], label_col: str) -> BanditGame: # pylint: disable=undefined-variable
+    def from_csv_reader(csv_reader: Iterable[List[str]], label_col: str) -> ContextGame: # pylint: disable=undefined-variable
         features: List[Sequence[str]] = []
         labels  : List[str]           = []
 
@@ -64,11 +71,13 @@ class BanditGame(Generic[T]):
                 features.append(row_vals[:label_index] + row_vals[(label_index+1):])
                 labels  .append(row_vals[label_index])
 
-        return BanditGame.from_classifier_data(features, labels)
+        return ContextGame.from_classifier_data(features, labels)
 
-    def __init__(self, rounds: Iterable[T]) -> None:
-        self._rounds = rounds
+    def __init__(self, rounds: Iterable[ContextRound]) -> None: 
+        super().__init__(rounds)
+        
+        #MyPy doesn't infer type correctly when overriding so explicit type is necessary
+        self._rounds: Iterable[ContextRound] = rounds 
 
-    @property
-    def rounds(self) -> Iterable[T]:
+    def rounds(self) -> Iterable[ContextRound]:
         return self._rounds
