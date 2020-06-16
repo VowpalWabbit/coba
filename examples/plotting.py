123cd782bc3f19934d9b9f9edf8c0614185cc6e4
Mark Rucker <rucker.mark@gmail.com>
2020-06-16 09:41:57 -0400

    Improving the benchmark plotting example

--- examples/plotting.py
+++ examples/plotting.py
@@ -13,13 +13,15 @@ from bbench.benchmarks import UniversalBenchmark
 #define a game
 game = LambdaGame(lambda i: None, lambda s: [0,1,2,3,4], lambda s,a: random.uniform(a-2, a+2))
 
-#create three different solvers
-randomsolver_factory = lambda: RandomSolver()
+#create three different solver factories
+randomsolver_factory   = lambda: RandomSolver()
 averagesolver_factory1 = lambda: EpsilonAverageSolver(1/10, lambda a: 0)
 averagesolver_factory2 = lambda: EpsilonAverageSolver(1/10, lambda a: 10)
 
 #define a benchmark
-benchmark = UniversalBenchmark([game], lambda i: 1, 100)
+#  the benchmark replays the game 30 times to average 
+#  out a solver randomly guessing the right answer
+benchmark = UniversalBenchmark([game]*30, lambda i: 1, 300)
 
 #benchmark all three solvers
 random_result   = benchmark.evaluate(randomsolver_factory)
@@ -34,9 +36,9 @@ ax.plot([ i.mean for i in random_result.progressive_stats]  , label="random")
 ax.plot([ i.mean for i in average_result1.progressive_stats], label="pessimistic epsilon-greedy")
 ax.plot([ i.mean for i in average_result2.progressive_stats], label="optimistic epsilon-greedy")
 
-ax.set_title("Mean Observed Reward for Iterations <= ")
+ax.set_title("Mean Observed Reward for Progressive Iterations")
 ax.set_ylabel("Mean Reward")
-ax.set_xlabel("Iteration <=")
+ax.set_xlabel("Progressive Iteration")
 
 ax.legend()
 plt.show()
