25ce35a3a525878d8ea1da0b215261b134fae082
Mark Rucker <rucker.mark@gmail.com>
2020-06-20 16:04:48 -0400

    better comments in the vowpal example

--- examples/vowpal.py
+++ examples/vowpal.py
@@ -68,8 +68,8 @@ averagesolver_factory2 = lambda: EpsilonAverageSolver(1/10, lambda a: 10)
 vowpalsolver_factory   = lambda: VowpalSolver()
 
 #define a benchmark
-#  the benchmark replays the game 15 times to average 
-#  out a solver randomly guessing the right answer early
+#  the benchmark replays the game 15 times in order to average
+#  out when a solver randomly guesses the right answer early
 benchmark = UniversalBenchmark([game]*15, lambda i: 1, 900)
 
 #benchmark all three solvers
@@ -78,17 +78,18 @@ average_result1 = benchmark.evaluate(averagesolver_factory1)
 average_result2 = benchmark.evaluate(averagesolver_factory2)
 vowpal_result   = benchmark.evaluate(vowpalsolver_factory)
 
+#plot the benchmark results
 fig = plt.figure()
 
 ax = fig.add_subplot(1,1,1)
 
-ax.plot([ i.mean for i in random_result.progressive_stats]  , label="random")
+ax.plot([ i.mean for i in random_result  .progressive_stats]  , label="random")
 ax.plot([ i.mean for i in average_result1.progressive_stats], label="pessimistic epsilon-greedy")
 ax.plot([ i.mean for i in average_result2.progressive_stats], label="optimistic epsilon-greedy")
-ax.plot([ i.mean for i in vowpal_result.progressive_stats]  , label="vowpal")
+ax.plot([ i.mean for i in vowpal_result  .progressive_stats]  , label="vowpal")
 
 ax.set_title("Mean Observed Reward for Progressive Iterations")
-ax.set_ylabel("Mean Reward")
+ax.set_ylabel("Mean Observed Reward")
 ax.set_xlabel("Progressive Iteration")
 
 ax.legend()
