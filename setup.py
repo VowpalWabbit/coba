c3a2963f84c3d41bdaf1e5205278f2cbcda61046
Mark Rucker <rucker.mark@gmail.com>
2020-11-16 12:35:56 -0500

    version bump

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 import setuptools
 
 MAJOR               = 1
-MINOR               = 6
-MICRO               = 5
+MINOR               = 7
+MICRO               = 0
 VERSION             = f"{MAJOR}.{MINOR}.{MICRO}"
 
 with open("README.md", "r") as f:
