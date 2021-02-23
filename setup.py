7e85219a4bac6b89629a508f596f1b42f8d1c0ff
Mark Rucker <rucker.mark@gmail.com>
2021-02-23 13:30:03 -0500

    Bumping version number for 2.0.7 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 MAJOR               = 2
 MINOR               = 0
-MICRO               = 6
+MICRO               = 7
 VERSION             = f"{MAJOR}.{MINOR}.{MICRO}"
 
 with open("README.md", "r") as f:
