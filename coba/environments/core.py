c41388f08098f4813938a09a115beb2e9a0cf38f
Mark Rucker <rucker.mark@gmail.com>
2021-11-11 09:35:07 -0500

    Small cleanup of SimulatedInteractions.

--- coba/environments/core.py
+++ coba/environments/core.py
@@ -1,5 +1,3 @@
-import collections
-
 from abc import abstractmethod, ABC
 from typing import Optional, Sequence, Hashable, Any, Union, Iterable, overload, Dict
 
@@ -13,10 +11,10 @@ class SimulatedInteraction:
     """A class to contain all data needed to represent an interaction in a simulated bandit interaction."""
 
     @overload
-    def __init__(self, 
-        context: Context, 
+    def __init__(self,
+        context: Context,
         actions: Sequence[Action],
-        *
+        *,
         rewards: Sequence[float],
         **kwargs: Sequence[Any]) -> None:
         ...
@@ -31,9 +29,10 @@ class SimulatedInteraction:
         """
 
     @overload
-    def __init__(self, 
-        context: Context, 
-        actions: Sequence[Action], *, 
+    def __init__(self,
+        context: Context,
+        actions: Sequence[Action], 
+        *,
         reveals: Sequence[Any],
         **kwargs: Sequence[Any]) -> None:
         ...
@@ -69,7 +68,8 @@ class SimulatedInteraction:
                 When working with non-scalar data use "reveals" instead of "rewards" to make it clear to Coba the data is non-scalar.
             **kwargs: Additional information that should be recorded in the interactions table of an experiment result. If any
                 data is a sequence with length equal to actions only the data at the selected action index will be recorded.
-        """    
+        """
+
     def __init__(self, *args, **kwargs) -> None:
 
         assert len(args) == 2, "An unexpected number of positional arguments was supplied to Interaction."
@@ -78,66 +78,20 @@ class SimulatedInteraction:
         assert "rewards" not in kwargs or len(args[1]) == len(kwargs["rewards"]), "Interaction rewards must match action length."
         assert "reveals" not in kwargs or len(args[1]) == len(kwargs["reveals"]), "Interaction reveals must match action length."
 
-        context = self._flatten(args[0])
-        actions = [ self._flatten(action) for action in args[1] ]
+        self._context = self._hashable(args[0])
+        self._actions = [ self._hashable(action) for action in args[1] ]
 
-        self._context =  context if not isinstance(context,dict) else HashableDict(context)
-        self._actions = [ action if not isinstance(action ,dict) else HashableDict(action) for action in actions ]
         self._kwargs  = kwargs
 
-    def _is_sparse(self, feats):
+    def _hashable(self, feats):
 
-        if isinstance(feats,dict):
-            return True
+        if isinstance(feats, dict):
+            return HashableDict(feats)
 
-        if not isinstance(feats, collections.Sequence):
-            return False
+        if isinstance(feats,list):
+            return tuple(feats)
 
-        if len(feats) != 2:
-            return False
-
-        if not isinstance(feats[0], collections.Sequence) or not isinstance(feats[1],collections.Sequence):
-            return False
-
-        if len(feats[0]) != len(feats[1]):
-            return False
-
-        if isinstance(feats[0],str) or isinstance(feats[1],str):
-            return False
-
-        return True
-
-    def _flatten(self, feats):
-
-        if not isinstance(feats, collections.Sequence) or isinstance(feats, (str,dict)):
-            return feats
-
-        if not self._is_sparse(feats):
-
-            flattened_dense_values = []
-
-            for val in feats:
-                if isinstance(val,(list,tuple,bytes)):
-                    flattened_dense_values.extend(val)
-                else:
-                    flattened_dense_values.append(val)
-            
-            return tuple(flattened_dense_values)
-        else:
-            keys = []
-            vals = []
-
-            for key,val in zip(*feats):
-
-                if isinstance(val, (list,tuple,bytes)):
-                    for sub_key,sub_val in enumerate(val):
-                        keys.append(f"{key}_{sub_key}")
-                        vals.append(sub_val)
-                else:
-                    keys.append(key)
-                    vals.append(val)
-
-            return HashableDict(zip(keys,vals))
+        return feats
 
     @property
     def context(self) -> Context:
@@ -155,11 +109,8 @@ class SimulatedInteraction:
     def kwargs(self) -> Dict[str,Any]:
         return self._kwargs
 
-
 class LoggedInteraction:
-    """
-    TODO: docs
-    """
+
     def __init__(self, context: Context, action: Action, reward: float, actions: Sequence[Action] = None, probability: float = None) -> None:
 
         self._context     = context
