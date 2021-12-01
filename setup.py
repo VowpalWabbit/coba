275d5c8020b9e8c3085c7c3a5a77cda02bdd88f0
Mark Rucker <rucker.mark@gmail.com>
2021-12-01 12:44:27 -0500

    Bumping micro version to release a few bug fixes.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 MAJOR               = 4
 MINOR               = 1
-MICRO               = 3
+MICRO               = 4
 VERSION             = f"{MAJOR}.{MINOR}.{MICRO}"
 
 with open("README.md", "r") as f:
