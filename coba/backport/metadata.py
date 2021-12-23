8097cf05dff7155ca6b5163f7ceb69cde52e5660
Mark Rucker <rucker.mark@gmail.com>
2021-12-23 17:52:09 -0500

    Fixing a backport error.

--- coba/backport/metadata.py
+++ coba/backport/metadata.py
@@ -1,7 +1,7 @@
 import sys
 
 if sys.version_info >= (3,8):# pragma: no cover
-    from importlib.metadata import entry_points
+    from importlib.metadata import entry_points, version
 else:
     from importlib_metadata import entry_points, version 
 
