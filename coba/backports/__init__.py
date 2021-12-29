10431130f080fde2952a8e9f846660947418141b
Mark Rucker <rucker.mark@gmail.com>
2021-12-29 14:02:21 -0500

    Removing broken/unused TypeAlias reference.

--- coba/backports/__init__.py
+++ coba/backports/__init__.py
@@ -1,9 +1,8 @@
 from coba.backports.metadata import version, entry_points
-from coba.backports.typing import Literal, TypeAlias
+from coba.backports.typing import Literal
 
 __all__ = [
     'version',
     'entry_points',
     'Literal',
-    'TypeAlias'
 ]
--- coba/backports/typing.py
+++ coba/backports/typing.py
@@ -4,8 +4,3 @@ if sys.version_info >= (3,8):# pragma: no cover
     from typing import Literal
 else:
     from typing_extensions import Literal
-
-if sys.version_info >= (3,10):# pragma: no cover
-    from typing import TypeAlias
-else:
-    from typing_extensions import TypeAlias
