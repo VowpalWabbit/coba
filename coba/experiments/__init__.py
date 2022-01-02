615e3dae99393aff40be2c5b7f719ffdbf3099cb
Mark Rucker <rucker.mark@gmail.com>
2022-01-02 18:55:29 -0500

    Small improvement to documentation.

--- coba/experiments/__init__.py
+++ coba/experiments/__init__.py
@@ -1,4 +1,4 @@
-"""The experiments module contains core experiment functionality and protocols.
+"""This module contains core experiment and result functionality.
 
 This module contains the Experiment evaluator, implementations of specific tasks that are performed
 during an experiment evaluation, and the Result datastructures returned after experiment evalution.
