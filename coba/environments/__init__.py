6423a1aa9d94ec243c9b6d7ff2c99430c296fbc9
Eduardo Salinas <edus@microsoft.com>
2022-01-06 15:12:59 -0500

    more reorder

--- coba/environments/__init__.py
+++ coba/environments/__init__.py
@@ -5,13 +5,13 @@ concrete implementations of these environments for use in experiments, and vario
 can be applied to environments to modify them in useful ways (e.g., shuffling, scaling, and imputing).
 """
 
-from coba.environments.core       import Environments
 from coba.environments.primitives import Context, Action, Interaction, Environment, ReaderEnvironment
 from coba.environments.logged.primitives import LoggedInteraction, LoggedEnvironment
 from coba.environments.filters    import Shuffle, Take, Identity, Reservoir
 from coba.environments.filters    import Sort, Scale, Cycle, Impute
 from coba.environments.filters    import Binary, WarmStart, Sparse
 from coba.environments.filters    import FilteredEnvironment, EnvironmentFilter
+from coba.environments.core       import Environments
 
 from coba.environments.simulated import SimulatedInteraction, SimulatedEnvironment
 from coba.environments.simulated import MemorySimulation, LambdaSimulation
