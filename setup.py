541bf7b3a100e8e893ac347c7322faa53f2eb3e5
Mark Rucker <rucker.mark@gmail.com>
2022-01-14 12:51:46 -0500

    Bumping version number for release.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 import setuptools
 
 MAJOR               = 4
-MINOR               = 5
-MICRO               = 1
+MINOR               = 6
+MICRO               = 0
 VERSION             = f"{MAJOR}.{MINOR}.{MICRO}"
 
 with open("README.md", "r") as f:
