59aa2f870b10414cbc71752efda3a79abe288d84
Mark Rucker <rucker.mark@gmail.com>
2022-01-30 17:46:45 -0500

    Improved First Script.py

--- examples/scripts/First Script.py
+++ examples/scripts/First Script.py
@@ -26,13 +26,13 @@ if __name__ == '__main__':
     ]
 
     #Next we create the environments we'd like evaluate against
-    environments = Environments.from_linear_synthetic(5000).shuffle([0,1,2]).binary()
+    environments = Environments.from_linear_synthetic(1000).shuffle([0,1,2]).binary()
 
     #We then create and evaluate our experiment from our environments and learners 
     result = Experiment(environments,learners).evaluate()
 
     #After evaluating can create a quick summary plot to get a sense of how the learners performed
-    result.plot_learners(err='sd')
+    result.plot_learners(err='sd',xlim=(10,1000))
 
     #We can also create a plot examining how specific learners did across each shuffle of our environments
-    result.filter_lrn(full_name="vw").plot_learners(err='sd',each=True)
+    result.filter_lrn(full_name="vw").plot_learners(err='sd',each=True,xlim=(10,1000))
