207d6d4a87e5dd3229b41ebc0179cd22022cfc81
Mark Rucker <rucker.mark@gmail.com>
2022-03-31 16:06:16 -0400

    Bumping version number to release perofmrance improvements for InteractionsEncoding.

--- coba/tests/test_coba.py
+++ coba/tests/test_coba.py
@@ -5,7 +5,7 @@ class Coba_Tests(unittest.TestCase):
 
     def test_version(self):
         from coba import __version__
-        self.assertEqual("4.6.11",__version__)
+        self.assertEqual("4.6.12",__version__)
     
 if __name__ == '__main__':
     unittest.main()
