2be60b10405841b70941911c1173565c2207020c
Mark Rucker <rucker.mark@gmail.com>
2022-05-04 18:10:25 -0400

    Bumped version number in preparation for release.

--- coba/tests/test_coba.py
+++ coba/tests/test_coba.py
@@ -5,7 +5,7 @@ class Coba_Tests(unittest.TestCase):
 
     def test_version(self):
         from coba import __version__
-        self.assertEqual("4.8.5",__version__)
+        self.assertEqual("4.8.6",__version__)
 
 if __name__ == '__main__':
     unittest.main()
