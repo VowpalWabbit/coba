46e9301b577f4eb3316d4cb50297704a71beab8a
Mark Rucker <rucker.mark@gmail.com>
2022-06-13 12:11:55 -0400

    Bumped version number to release bug fix.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 MAJOR               = 4
 MINOR               = 10
-MICRO               = 1
+MICRO               = 2
 VERSION             = f"{MAJOR}.{MINOR}.{MICRO}"
 
 with open("README.md", "r") as f:
