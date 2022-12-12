5d62f93daa84068ad156c401f88249ab268c2790
Mark Rucker <rucker.mark@gmail.com>
2022-12-11 21:25:23 -0500

    Updating version number for release.

--- coba/tests/test_coba.py
+++ coba/tests/test_coba.py
@@ -5,7 +5,7 @@ class Coba_Tests(unittest.TestCase):
 
     def test_version(self):
         from coba import __version__
-        self.assertEqual("6.0.1",__version__)
+        self.assertEqual("6.1.0",__version__)
 
 if __name__ == '__main__':
     unittest.main()
