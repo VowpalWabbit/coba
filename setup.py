a387b6147c76a9f2ff47aaf63e1687069a8d4ba9
Mark Rucker <rucker.mark@gmail.com>
2023-01-17 18:11:41 -0500

    Bumped micro version to deploy a bug fix.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 MAJOR               = 6
 MINOR               = 2
-MICRO               = 4
+MICRO               = 5
 VERSION             = f"{MAJOR}.{MINOR}.{MICRO}"
 
 with open("README.md", "r") as f:
